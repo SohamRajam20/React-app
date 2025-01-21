13ff3b9566436d6368894364e02f6f8599956faa
SohamRajam20 <sohamrajam20@gmail.com>
2025-01-21 11:40:04 +0530

    react-app

---
+++ my-react-app/src/App.jsx
@@ -0,0 +1,14 @@
+import Test from './components/test'; // Import the Test component
+function App() {
+  
+
+  return (
+    <>
+     <div>
+       <Test />
+     </div>
+    </>
+  )
+}
+
+export default App
